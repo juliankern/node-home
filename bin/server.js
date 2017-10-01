30583f9290faab7a10ab07b2d7f7299daf892185
Julian Kern <mail@juliankern.com>
2017-10-01 13:56:02 +0200

    feat-childprocess | server and client now start a child process by themselves, and restart it if it crashes

--- bin/server.js
+++ bin/server.js
@@ -10,13 +10,15 @@ if (+process.version.replace('v', '').split('.')[0] < 8) {
 
 require('../util/global.js');
 
+const pkg = global.req('package.json');
+
 const Logger = global.req('classes/Log.class');
-const logger = new Logger();
+const masterlogger = new Logger('master');
 
 console.log(''); // eslint-disable-line no-console
 
-const pkg = global.req('package.json');
 const cli = require('cli');
+const cluster = require('cluster');
 
 cli.enable('version');
 cli.setApp('bin/server.js', pkg.version);
@@ -24,26 +26,94 @@ cli.setApp('bin/server.js', pkg.version);
 const cliOptions = cli.parse({
     port: ['p', 'Websocket port to use instead of autodiscover', 'int', null],
     web: ['w', 'Port for the website to use', 'int', null],
-    nobonjour: ['n', 'Disable bonjour to let clients connect by the server address', 'bool', false],
+    nobonjour: [false, 'Disable bonjour to let clients connect by the server address', 'bool', false],
+    disableRestart: [false, 'Disable auto-restart of server if it crashes', 'bool', false],
+    restartDelay: [false, 'How long the restart should be delayed after exit', 'int', 1000],
+    maxRestarts: [false, 'Maximum number of automatic restarts before it should stop - set to -1 to disable', 'int', 5],
 });
 
 // ////////////////////////////////////////////////////////
 
-const SmartNodeServer = new (global.req('classes/Server.class.js'))(() => {
-    SmartNodeServer
-        .init(cliOptions)
-        .catch((e) => { logger.error('Server init error', e); });
-});
+if (cluster.isMaster) {
+    let restartCount = 0;
+    let forcedRestart = false;
 
-// ////////////////////////////////////////////////////////
+    // eslint-disable-next-line global-require
+    if (process.title === 'npm' && require('os').type().includes('Windows')) {
+        masterlogger.warn(`If you want to see the fontend, you'll need to run 
+            "npm run watch-scss" as well to compile CSS!`);
+        console.log(''); // eslint-disable-line no-console
+    }
 
-// eslint-disable-next-line global-require
-if (process.title === 'npm' && require('os').type().includes('Windows')) {
-    logger.warn('If you want to see the fontend, you\'ll need to run "npm run watch-scss" as well to compile CSS!');
-    console.log(''); // eslint-disable-line no-console
+    // ////////////////////////////////////////////////////////
+    masterlogger.info(`Master server process is running with ID ${process.pid}`);
+
+    let child = cluster.fork();
+
+    cluster.on('exit', (worker) => {
+        if (forcedRestart) {
+            child = restartChild();
+            forcedRestart = false;
+            restartCount = 0;
+        } else {
+            masterlogger.warn(`Child with ID ${worker.process.pid} exited`);
+
+            if (!cliOptions.disableRestart &&
+                (
+                    cliOptions.maxRestarts === -1 ||
+                    cliOptions.maxRestarts > restartCount
+                )
+            ) {
+                setTimeout(() => {
+                    child = restartChild();
+                }, cliOptions.restartDelay);
+
+                restartCount++;
+            }
+        }
+    });
+
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', (data) => {
+        if ((`${data}`).trim().toLowerCase() === 'rs') {
+            masterlogger.info('Restarting server process NOW!');
+            child.send('exit');
+            forcedRestart = true;
+        }
+    });
+
+    process.on('SIGINT', () => {
+        masterlogger.warn('SIGINT caught!!');
+        child.send('exit');
+    });
+} else {
+    const logger = new Logger('child');
+
+    logger.info(`Child process started with ID ${process.pid}`);
+
+    const SmartNodeServer = new (global.req('classes/Server.class.js'))(() => {
+        SmartNodeServer
+            .init(cliOptions)
+            .catch((e) => { logger.error('Server init error', e); });
+    });
+
+    // do something when app is closing
+    process.on('message', (msg) => {
+        if (msg === 'exit') {
+            exitHandler.call([SmartNodeServer, logger]);
+        }
+    });
+    process.on('exit', exitHandler.bind([SmartNodeServer, logger]));
+    process.on('SIGINT', exitHandler.bind([SmartNodeServer, logger]));
+    process.on('uncaughtException', exitHandler.bind([SmartNodeServer, logger]));
 }
 
 // ////////////////////////////////////////////////////////
+
+function restartChild() {
+    return cluster.fork();
+}
 
 /**
  * exit handler for cleanup and stuff
@@ -53,6 +123,7 @@ if (process.title === 'npm' && require('os').type().includes('Windows')) {
  * @param  {object} err Holding the error messages
  */
 function exitHandler(err) {
+    const [SmartNodeServer, logger] = this;
     logger.info('SmartNode exiting...');
 
     if (err) {
@@ -61,8 +132,3 @@ function exitHandler(err) {
 
     SmartNodeServer.close(process.exit);
 }
-
-// do something when app is closing
-process.on('exit', exitHandler);
-process.on('SIGINT', exitHandler);
-process.on('uncaughtException', exitHandler);
